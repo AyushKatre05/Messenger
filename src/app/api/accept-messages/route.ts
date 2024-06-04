13d3f6bca454c6f00f058fba71feb247e50a4218
Ayushatre05 <ayushkatre1801@gmail.com>
2024-06-04 19:58:37 +0530

    message Api generated using aggregation pipeline

--- src/app/api/accept-messages/route.ts
+++ src/app/api/accept-messages/route.ts
@@ -5,7 +5,6 @@ import UserModel from '@/model/User';
 import { User } from 'next-auth';
 
 export async function POST(request: Request) {
-  // Connect to the database
   await dbConnect();
 
   const session = await getServerSession(authOptions);
@@ -21,7 +20,6 @@ export async function POST(request: Request) {
   const { acceptMessages } = await request.json();
 
   try {
-    // Update the user's message acceptance status
     const updatedUser = await UserModel.findByIdAndUpdate(
       userId,
       { isAcceptingMessages: acceptMessages },
@@ -29,7 +27,6 @@ export async function POST(request: Request) {
     );
 
     if (!updatedUser) {
-      // User not found
       return Response.json(
         {
           success: false,
@@ -38,8 +35,6 @@ export async function POST(request: Request) {
         { status: 404 }
       );
     }
-
-    // Successfully updated message acceptance status
     return Response.json(
       {
         success: true,
@@ -56,17 +51,10 @@ export async function POST(request: Request) {
     );
   }
 }
-
-
 export async function GET(request: Request) {
-  // Connect to the database
   await dbConnect();
-
-  // Get the user session
   const session = await getServerSession(authOptions);
   const user = session?.user;
-
-  // Check if the user is authenticated
   if (!session || !user) {
     return Response.json(
       { success: false, message: 'Not authenticated' },
@@ -75,18 +63,13 @@ export async function GET(request: Request) {
   }
 
   try {
-    // Retrieve the user from the database using the ID
     const foundUser = await UserModel.findById(user._id);
-
     if (!foundUser) {
-      // User not found
       return Response.json(
         { success: false, message: 'User not found' },
         { status: 404 }
       );
     }
-
-    // Return the user's message acceptance status
     return Response.json(
       {
         success: true,
---
+++ src/app/api/send-message/route.ts
@@ -0,0 +1,37 @@
+import UserModel from '@/model/User';
+import dbConnect from '@/lib/dbConnect';
+import { Message } from '@/model/User';
+
+export async function POST(request: Request) {
+  await dbConnect();
+  const { username, content } = await request.json();
+  try {
+    const user = await UserModel.findOne({ username }).exec();
+    if (!user) {
+      return Response.json(
+        { message: 'User not found', success: false },
+        { status: 404 }
+      );
+    }
+    if (!user.isAcceptingMessage) {
+      return Response.json(
+        { message: 'User is not accepting messages', success: false },
+        { status: 403 }
+      );
+    }
+    const newMessage = { content, createdAt: new Date() };
+    user.messages.push(newMessage as Message);
+    await user.save();
+
+    return Response.json(
+      { message: 'Message sent successfully', success: true },
+      { status: 201 }
+    );
+  } catch (error) {
+    console.error('Error adding message:', error);
+    return Response.json(
+      { message: 'Internal server error', success: false },
+      { status: 500 }
+    );
+  }
+}
